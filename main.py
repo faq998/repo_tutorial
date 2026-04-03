a8da002dee624752a3182b37328e83e6b5b9caaf
Facundo Avellaneda <168240955+faq998@users.noreply.github.com>
2026-04-03 10:22:32 -0300

    Delete main.py
    se elimina
    
    Signed-off-by: Facundo Avellaneda <168240955+faq998@users.noreply.github.com>

--- main.py
+++
@@ -1,6 +0,0 @@
-print("Hola Mundo")
-nombre= input("Ingrese su nombre: ").strip().upper()
-while not nombre.isalpha():
-    nombre=input("Se aceptan unicamente letras, escriba correctamente: ")
-
-print(f"Hola,{nombre}")
